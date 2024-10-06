aaed411d08ffa071c1b3fbac59b8fc330255ffb4
unknown <mejbacsedu@gmail.com>
2024-10-06 22:19:36 +0600

    PrivateRoute

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../../components/Banner/Banner";
+import HowToUse from "../../../components/HowToUse/HowToUse";
 import SectionHeading from "../../../components/SectionHeading/SectionHeading";
 import ShowCards from "../../../components/ShowCards/ShowCards";
 
@@ -6,6 +7,10 @@ export default function Home() {
   return (
     <div>
       <Banner />
+      <div className="lg:conatiner mx-auto px-8">
+        <SectionHeading heading="How To Use"></SectionHeading>
+        <HowToUse></HowToUse>
+      </div>
       <div className="lg:conatiner mx-auto px-8">
         <SectionHeading heading="Books Available"></SectionHeading>
         <ShowCards></ShowCards>
